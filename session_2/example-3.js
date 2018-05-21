5b888f91aae09a70c77ab5ec79e4322dcc76fd06
Pantoji, Siddhiraj <siddhiraj.pantoji@travelex.com>
2018-05-21 15:25:18 +0530

    Changes for Prime Number and Callbacks

---
+++ session_2/example-3.js
@@ -0,0 +1,7 @@
+
+function isPrimeNumber(num) {
+    // Code your logic here for Prime number and lets code together 
+   
+}
+
+console.log(isPrimeNumber(0));
---
+++ session_2/example-4.js
@@ -0,0 +1,6 @@
+function divide(a, b )
+{
+    return a/b;
+}
+
+console.log(divide(6,3));
