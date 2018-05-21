6dde389816a23e94def78622090b7062a2f0a6b3
Pantoji, Siddhiraj <siddhiraj.pantoji@travelex.com>
2018-05-21 14:45:25 +0530

    Changes for Session Start

--- session_2/example-1.js
+++ session_2/example-1.js
@@ -1,24 +1,11 @@
 var abc = "Hello"
 console.log(abc)
 
-function pqr(abc)
-{
-    
+function pqr(){
+    console.log("Inside PQR")
     abc = "Hi "
- //   this.abc = abc;
-    console.log("Inside pQR")
     console.log(abc);
-    console.log(this.abc);
-    
 }
-pqr(abc);
-var xyz = {
-    cvb:"X",
-    prq:function(cvb){
-        console.log(this.cvb)
-        console.log(cvb)
-    }
-}
-xyz.prq("Y");
-console.log("After PQR Call ");
+
+pqr();
 console.log(abc);
