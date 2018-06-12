18d17b65d2aaaa711bc98a1b797d1d242a6ac3a4
Pantoji, Siddhiraj <siddhiraj.pantoji@travelex.com>
2018-06-12 19:13:04 +0530

    Changes for Session 5

--- session_4/example1.js
+++ session_4/example1.js
@@ -4,4 +4,7 @@ console.log( typeof obj);
 var xyz = JSON.parse(obj); // Parsing Json
 console.log(typeof xyz); 
 console.log(xyz); 
+console.log(xyz.abc);
+console.log(obj.abc);
+console.log("Foram")
 console.log(JSON.stringify(xyz));
--- session_4/example3.js
+++ session_4/example3.js
@@ -4,4 +4,5 @@ var app = express();
 app.get('/getResponse',(req, res) => {
     res.contentType("applicaiton/json");
     res.send("{message:'this is test message'}")
-}).listen(4000);
+});
+app.listen(4000);
