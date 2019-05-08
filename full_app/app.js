18aa162893ee96a15b9f14030c139c3d90ce1e9b
Pantoji, Siddhiraj <siddhiraj.pantoji@travelex.com>
2019-05-08 15:47:19 +0530

    Changes for Validators and Validations

--- full_app/app.js
+++ full_app/app.js
@@ -1,6 +1,8 @@
 // This will Contain our express app code 
 var express = require('express')
+const expressValidator= require('express-validator'); // 
 var app = express();
+app.use(expressValidator());
 app.use(require('body-parser').json());
 var loginRoutes = require('./routers/loginRouter');
 var emplRoutes = require('./routers/employee-router')
